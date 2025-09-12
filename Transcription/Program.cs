ea527616b62cd0fcd1b602a8765f3e0d5918dede
EnderEzhik <Achernov2007@gmail.com>
2025-09-12 19:39:45 +0300

    Инициализирован проект, отвечающий за основную логику сервиса - работа с транскрипциями

---
+++ Transcription/Program.cs
@@ -0,0 +1,15 @@
+namespace Transcription;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        app.Run();
+    }
+}
