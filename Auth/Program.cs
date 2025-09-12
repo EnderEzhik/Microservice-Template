64eb8d4931ebe1c8e33f1b8d531e0d05d02c0005
EnderEzhik <Achernov2007@gmail.com>
2025-09-12 19:43:44 +0300

    Инициализирован проект аутентификации

---
+++ Auth/Program.cs
@@ -0,0 +1,17 @@
+namespace Auth;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
