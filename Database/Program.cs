1928f726a8df7db338399ba391d0192a23e953fe
EnderEzhik <Achernov2007@gmail.com>
2025-09-12 19:33:13 +0300

    Инициализирован проект базы данных

---
+++ Database/Program.cs
@@ -0,0 +1,15 @@
+namespace Database;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        app.Run();
+    }
+}
