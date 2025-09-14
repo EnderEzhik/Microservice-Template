461d37da5b680cc3136b2034816fb38a4f8e4220
EnderEzhik <Achernov2007@gmail.com>
2025-09-14 21:53:23 +0300

    Сервис базы данных: Добавлен ендпоинт для получения транскрипции по url из базы данных. Добавлен ендпоинт для сохранения транскрипции в базу данных.

--- Database/Program.cs
+++ Database/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Database.Data;
@@ -23,6 +25,55 @@ public class Program
             var app = builder.Build();
 
             app.UseSerilogRequestLogging();
+
+            app.MapGet("transcription", async (MyDbContext db, [FromQuery] string url) =>
+            {
+                Log.Information("GET request for get transcription for url: {Url}", url);
+
+                try
+                {
+                    var transcription = await db.Transcriptions.FirstOrDefaultAsync(t => t.Url == url);
+
+                    if (transcription != null)
+                    {
+                        Log.Information("Transcription found for url: {Url}", url);
+                        return Results.Ok(transcription);
+                    }
+                    
+                    Log.Information("Transcription not found for url: {Url}", url);
+                    return  Results.NotFound();
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "Error getting transcription for url: {Url}", url);
+                    throw;
+                }
+            });
+            
+            app.MapPost("transcription", async (MyDbContext db, CreateTranscriptionRequest request) =>
+            {
+                Log.Information("POST request for save transcription for url: {Url}", request.Url);
+
+                try
+                {
+                    var newTranscription = new Shared.Models.Transcription()
+                    {
+                        Url = request.Url,
+                        Content = request.Content
+                    };
+                    
+                    db.Transcriptions.Add(newTranscription);
+                    await db.SaveChangesAsync();
+                    
+                    Log.Information("Transcription saved to db for url: {Url}", request.Url);
+                    return Results.Created();
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "Error saving transcription for url: {Url}", request.Url);
+                    throw;
+                }
+            });
 
             Log.Information("Database service configured successfully");
             app.Run();
@@ -63,4 +114,13 @@ public class Program
 
         Log.Information("Database connection established");
     }
+}
+
+record CreateTranscriptionRequest
+{
+    [JsonPropertyName("url")]
+    public string Url { get; init; }
+    
+    [JsonPropertyName("content")]
+    public string Content { get; init; }
 }
