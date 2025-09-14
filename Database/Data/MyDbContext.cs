df0b414a4c4be319124ec4c2481342f16ad9c696
EnderEzhik <Achernov2007@gmail.com>
2025-09-14 21:21:24 +0300

    Сервис базы данных: Сущность транскрипции добавлена в контекст базы данных.

--- Database/Data/MyDbContext.cs
+++ Database/Data/MyDbContext.cs
@@ -1,8 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace Database.Data;
 
 public class MyDbContext : DbContext
 {
+    private static readonly Serilog.ILogger logger = Log.ForContext<MyDbContext>();
+    
+    public DbSet<Shared.Models.Transcription> Transcriptions { get; set; }
+    
     public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        logger.Debug("Configuring database model");
+        
+        modelBuilder.Entity<Shared.Models.Transcription>().HasIndex(t => t.Url);
+        
+        logger.Debug("Database model configured successfully");
+        base.OnModelCreating(modelBuilder);
+    }
 }
