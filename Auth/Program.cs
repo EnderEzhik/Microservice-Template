30ef2193db206f07af8e9b827dc36c2cd299cc3b
EnderEzhik <Achernov2007@gmail.com>
2025-09-19 21:59:00 +0300

    Сервис авторизации: Логгирование разделено на 2 файла - подробное с системными записями и основное, включающее только ручные записи. Рефакторинг.

--- Auth/Program.cs
+++ Auth/Program.cs
@@ -1,4 +1,6 @@
+using Auth.Services;
 using Serilog;
+using Serilog.Filters;
 
 namespace Auth;
 
@@ -11,14 +13,14 @@ public class Program
         try
         {
             Log.Information("Starting Auth service...");
-            
+
             var builder = WebApplication.CreateBuilder(args);
-            
+
             ConfigureServices(builder.Services);
 
             var app = builder.Build();
 
-            app.UseSerilogRequestLogging();
+            // app.UseSerilogRequestLogging();
             app.MapControllers();
 
             Log.Information("Auth service configured successfully");
@@ -35,24 +37,41 @@ public class Program
         }
     }
 
+    private static void ConfigureLogging()
+    {   
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .Enrich.FromLogContext()
+            .WriteTo.File(
+                path: "logs/all/auth-service-all.log",
+                rollingInterval: RollingInterval.Day,
+                shared: true)
+            .WriteTo.Logger(lc => lc
+                .Enrich.FromLogContext()
+                .Filter.ByExcluding(Matching.FromSource("System"))
+                .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+                .Filter.ByExcluding(Matching.FromSource("Serilog.AspNetCore"))
+                .WriteTo.File(
+                    path: "logs/auth-service.log",
+                    rollingInterval: RollingInterval.Day,
+                    shared: true,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}"))
+            .CreateLogger();
+    }
+
     private static void ConfigureServices(IServiceCollection services)
     {
         services.AddSerilog();
         services.AddHttpClient();
-        services.AddControllers();
-    }
 
-    private static void ConfigureLogging()
-    {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .WriteTo.Console(
-                outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-            .WriteTo.File(
-                path: "logs/auth-service.log",
-                rollingInterval: RollingInterval.Day,
-                retainedFileCountLimit: 7,
-                outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-            .CreateLogger();
+        var databaseServiceUrl = Environment.GetEnvironmentVariable("DATABASE_SERVICE_URL") ??
+                                 throw new InvalidOperationException("Missing environment variable DATABASE_SERVICE_URL");
+
+        services.AddHttpClient<DatabaseService>(client => { client.BaseAddress = new Uri(databaseServiceUrl); });
+
+        services.AddScoped<SubscriptionService>();
+        services.AddScoped<PaymentService>();
+
+        services.AddControllers();
     }
 }
